a57fd442237625adefe30c602cb5bac0bd58dd71
Yellow Club 5 <hello@yellowclub.by>
2024-04-28 13:08:06 +0300

    lesson 1 -- Creating echo bot

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-print("File: Alexander_Nejem")
+import asyncio
+from aiogram import types
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
 
 
+TOKEN = '6558387187:AAFoSJPpEna_rrS8pJk9fjI1Juh5uHGBOJ0'
+# связываем код с ботом в телеграме
+bot = Bot(token=TOKEN)
+
+# создаем диспетчер для обработки команд
+dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("Привет, это бот фастфуд")
 
 
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("бот находться в разработке")
+    user_text = message.text
+    await message.answer(user_text)
+
+
+# функция для старта бота
+async def main():
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
